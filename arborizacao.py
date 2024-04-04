abe00ef4eb55b4ec1e72bd2a444327cf53eb1791
Tiago Tavares <101755658+tiagotavarescodes@users.noreply.github.com>
2024-04-04 13:10:27 -0300

    Update arborizacao.py

--- arborizacao.py
+++ arborizacao.py
@@ -8,42 +8,34 @@ Original file is located at
 """
 
 import streamlit as st
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
+# Carregar o modelo e as classes
+model = tf.keras.models.load_model("keras_model.h5")
+class_names = open("labels.txt", "r").read().splitlines()
 
-# Load the model
-model = load_model("/arborizacao/keras_model.h5", compile=False)
-
-# Define class names
-class_names = ["Não", "Sim"]
-
+# Função para pré-processar a imagem
 def preprocess_image(image):
-    # Redimensionar a imagem para 224x224 e recortar do centro
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
-    # Converter a imagem em uma matriz numpy
     image_array = np.asarray(image)
-    # Normalizar a matriz da imagem
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-    # Expandir a dimensão para atender aos requisitos do modelo (1, 224, 224, 3)
     return np.expand_dims(normalized_image_array, axis=0)
 
+# Função para prever a classe da imagem
 def predict(image):
-    # Pré-processar a imagem
     data = preprocess_image(image)
-    # Prever a classe
     prediction = model.predict(data)
     index = np.argmax(prediction)
     class_name = class_names[index]
     confidence_score = prediction[0][index]
     return class_name, confidence_score
 
+# Função principal do Streamlit
 def main():
-    st.title("Classificador de Imagens Sim/Não")
+    st.title("Classificador de Imagens")
 
     uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "png", "jpeg"])
 
